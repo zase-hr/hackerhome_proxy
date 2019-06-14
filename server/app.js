24b1f2998cf41036832a5e5540f1c6deea34e570
ericdo <ericdo.617@gmail.com>
2019-06-14 14:50:58 -0700

    initial changes

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ for (route of routes) {
   app.use(route.route,
     proxy({
       target: route.address,
-      pathRewrite: (path, req) => {
+      pathRewrite: (path, req) => {     
         return path.split('/').slice(2).join('/'); // Could use replace, but take care of the leading '/'
       }
     })
